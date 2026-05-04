71f46001fe0d4209d6cb23b628852ecd6ccfd9e4
nicoamaya3 <nicolasatehortuaamaya@gmail.com>
2026-05-04 13:17:31 -0500

    status kevin

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import StatusJuan from './Components/StatusJuan'
+import StatusKevin from './Components/StatusKevin'
 
 const App = () => {
   return (
     <StatusJuan/>
+    <StatusKevin/>
   )
 }
 
