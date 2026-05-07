7191ae03c00a839cbc3af46138648f9a12f85da7
Luisach2020 <luisachavarria.987@gmail.com>
2026-05-07 12:51:18 -0500

    StatusJuan

--- src/Components/StatusJuan.jsx
+++ src/Components/StatusJuan.jsx
@@ -1,8 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const StatusJuan = () => {
+  const [corazon, setcorazon] = useState(0)  
   return (
+    <>
+    <div className='gemas'>  
+      <h1>corazones: {corazon}</h1>   
+        <button onClick={ () => { setcorazon( (prev) => {prev + 2} ) } }>1</button>
+        <button onClick={ () => { setcorazon( () => {} ) } }>2</button>        
+        <button onClick={ () => { setcorazon( () => {} ) } }>3</button>
+        <button onClick={ () => { setcorazon ( () => {} ) } }>4</button>
+</div>
+
+
+
+
+
+
+
+
+
+
     <div>StatusJuan</div>
+    </>
   )
 }
 
