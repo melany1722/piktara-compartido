20d42df2af4e38f311f599a189581e668be0baa8
Luisach2020 <luisachavarria.987@gmail.com>
2026-05-07 12:55:25 -0500

    getCharacters creado

--- src/Components/StatusJuan.jsx
+++ src/Components/StatusJuan.jsx
@@ -1,7 +1,19 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 const StatusJuan = () => {
   const [corazon, setcorazon] = useState(0)  
+
+  const getCharacters = async () => {
+
+    const res = await fetch('https://rickandmortyapi.com/api/character')
+    const data = await res.json()
+   
+  }
+
+  useEffect(() => {
+    getCharacters()
+  }, [])
+  
   return (
     <>
     <div className='gemas'>  
