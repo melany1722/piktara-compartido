44d71264f1671bf3be729c8836fb9be187f81238
SneyderG <sneyder.gaviria180107@gmail.com>
2026-05-07 12:44:58 -0500

    Statuskevin

--- src/Components/StatusKevin.jsx
+++ src/Components/StatusKevin.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
 
 const StatusKevin = () => {
+    const [puntos, setpuntos] = useState(0)      
+
+
+
   return (
+    <>
+    <div className='puntos'>  
+      <h1>Puntos: {puntos}</h1>   
+        <button onClick={ () => { setpuntos( (prev) => {prev + 2} ) } }>puntos +10</button>
+        <button onClick={ () => { setpuntos( () => {} ) } }>puntos +20</button>        
+        <button onClick={ () => { setpuntos( () => {} ) } }>puntos +30</button>
+        <button onClick={ () => {setpuntos( () => {} ) } }>puntos +40</button>
+</div>
+
+
+
+
+
+
     <div>StatusKevin</div>
+    </>
+    
   )
 }
 
