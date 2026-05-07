09036105e3420679b646771bd928997c3b6d5a55
melany1722 <melany17beltran22@mail.com>
2026-05-06 20:47:54 -0500

    cambios

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import StatusJuan from './Components/StatusJuan'
 import StatusKevin from './Components/StatusKevin'
+import StatusMelany from './Components/StatusMelany'
 
 const App = () => {
   return (
     <>
     <StatusJuan/>
     <StatusKevin/>
+    <StatusMelany/>
 
 </>
 
--- src/Components/StatusKevin.jsx
+++ src/Components/StatusKevin.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const StatusKevin = () => {
   return (
-    <StatusKevin/>
+    <div>StatusKevin</div>
   )
 }
 
