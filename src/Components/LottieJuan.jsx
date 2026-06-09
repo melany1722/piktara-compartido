e89cfd8fbcc979df600bec2cd55a8dd9be4c3bcb
juanLS07 <juanjose789t@gmail.com>
2026-06-09 17:00:04 -0500

    animacion-terminada

--- src/Components/LottieJuan.jsx
+++ src/Components/LottieJuan.jsx
@@ -1,9 +1,31 @@
 import LottieModule from "lottie-react";
 import circuloj from "../assets/animacionjuan.json";
+import { useRef } from "react";
 
 const Lottie = LottieModule.default;
 
 export const LottieJuan = () => {
-  return <Lottie animationData={circuloj} />;
+
+  const lottieRef = useRef()
+
+  const reproducir = () => {
+    lottieRef.current.play()
+  }
+  return (
+    <>
+      <div onClick={reproducir}>
+        <Lottie
+          lottieRef={lottieRef}
+          animationData={circuloj}
+          loop={false}
+          autoplay={false}
+          style={{
+            width: 500,
+            height: 500
+          }}
+        />
+      </div>
+    </>
+  );
 };
 
