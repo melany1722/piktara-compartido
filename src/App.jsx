be9f179cdd3d6a2e26b87d7d6a09468bbb09ebe1
melany1722 <melany17beltran22@mail.com>
2026-06-18 13:20:28 -0500

    Avances

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,24 @@
 import React from 'react'
-import {LottieJuan} from './Components/LottieJuan'
 import NuestroComic from './Pages/NuestroComic'
 import SobrePiktara from './Pages/SobrePiktara'
 import Home from './Pages/Home'
 import Trailer from './Pages/Trailer'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { LottieMelany } from './Components/LottieMelany'
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/lottiej" element={<LottieJuan/>} />
         <Route path="/" element={<Home />} />
         <Route path="/sobre-piktara" element={<SobrePiktara />} />
         <Route path="/nuestro-comic" element={<Trailer />} />
         <Route path="/sobre-el-proyecto" element={<NuestroComic />} />
+        <Route path="/melany" element={<LottieMelany />} />
       </Routes>
     </BrowserRouter>
   )
+  
 }
 
 export default App
