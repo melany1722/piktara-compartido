4d8d62ec4a021472ecf5de9720e1a2aa489e26b8
juanLS07 <juanjose789t@gmail.com>
2026-06-23 17:06:26 -0500

    junajjaja

--- src/Pages/Comic.jsx
+++ src/Pages/Comic.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import { Link } from "react-router-dom";
 import Buscar from "./Buscar";
+import "../Comic.css";
+import fondodesierto1 from "../assets/fondodesierto1.jpg";
 
 const Comic = () => {
   return (
-    <div></div>
+    <div className="min-vh-100 w-100 fondo-desierto">
+
+    </div>
   )
 }
 
